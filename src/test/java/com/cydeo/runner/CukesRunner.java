69280a7890354ea97398d83719ce2b530ed0c057
Salma Sali <salianovaj@gmail.com>
2022-11-08 11:45:19 -0500

    "json:target/cucumber.json" plugin added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-            "html:target/cucumber-report.html",
-            "rerun:target/rerun.txt",
-            "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
